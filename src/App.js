00bad4633028e23ab226e486313946ed3b1bd90d
fanhl <dyguests@qq.com>
2018-10-04 22:43:50 +0800

    image

--- src/App.js
+++ src/App.js
@@ -7,14 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Image, Platform, StyleSheet, Text, View,} from 'react-native';
-
-const instructions = Platform.select({
-    ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-    android:
-        'Double tap R on your keyboard to reload,\n' +
-        'Shake or press menu button for dev menu',
-});
+import {Image, StyleSheet, Text, View,} from 'react-native';
 
 type Props = {};
 
@@ -33,27 +26,21 @@ export default class App extends Component<Props> {
             <View style={styles.container}>
                 <Text>{movie.title}</Text>
                 <Text>{movie.year}</Text>
-                <Image source={{uri: movie.posters.thumbnail}}/>
+                <Image source={{uri: movie.posters.thumbnail}} style={styles.thumbnail}/>
             </View>
         );
     }
 }
 
-const styles = StyleSheet.create({
+var styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#F5FCFF"
     },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    },
+    thumbnail: {
+        width: 53,
+        height: 81
+    }
 });
