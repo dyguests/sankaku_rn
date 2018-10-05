ca2ed184df22299b441e3c95ab85dc90022e9421
fanhl <dyguests@qq.com>
2018-10-05 15:25:05 +0800

    MainScreen

--- src/App.js
+++ src/App.js
@@ -6,43 +6,13 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Image, StyleSheet, Text, View,} from 'react-native';
+import React from 'react';
 import {createStackNavigator} from 'react-navigation';
+import MainScreen from "./ui/main/MainScreen";
 
-type Props = {};
-
-let MOCKED_MOVIES_DATA = [
-    {
-        title: "标题",
-        year: "2015",
-        posters: {thumbnail: "http://i.imgur.com/UePbdph.jpg"}
-    }
-];
-
-export default class App extends Component<Props> {
-    render() {
-        let movie = MOCKED_MOVIES_DATA[0];
-        return (
-            <View style={styles.container}>
-                <Text>{movie.title}</Text>
-                <Text>{movie.year}</Text>
-                <Image source={{uri: movie.posters.thumbnail}} style={styles.thumbnail}/>
-            </View>
-        );
-    }
-}
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#F5FCFF"
-    },
-    thumbnail: {
-        width: 53,
-        height: 81
-    }
+const App = createStackNavigator({
+    Home: {screen: MainScreen},
+    // Profile: {screen: ProfileScreen},
 });
 
+export default App;
